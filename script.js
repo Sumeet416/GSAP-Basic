69cd33f9e4acc18a7df4d3879a040d759f9d561d
Sumeet416 <sumeet.17758@sakec.ac.in>
2025-05-26 18:06:55 +0530

    GSAP basics and scroll Trigger property used

--- script.js
+++ script.js
@@ -1,71 +1,70 @@
-// gsap.to(".box1", {
-//     y:400,
-//     duration:1,
-//     delay:1,
-//     rotate:360,
-//     scale:0.5,
-//     borderRadius:"50%",
-//     repeat:1,
-//     yoyo:true
-// })
-
-
-// gsap.to(".box2", {
-//     y:400,
-//     duration:1,
-//     delay:1,
-//     rotate:360,
-//     scale:0.5,
-//     borderRadius:"50%",
-//     repeat:1,
-//     yoyo:true
-// })
-
-// gsap.to(".box3", {
-//     y:400,
-//     duration:1,
-//     delay:1,
-//     rotate:360,
-//     scale:0.5,
-//     borderRadius:"50%",
-//     repeat:1,
-//     yoyo:true
-// })
-
-
 tl = gsap.timeline()
-
-// tl.from("#logo", {
-//     y:-30,
-//     opacity:0,
-//     duration:0.5,
-//     delay:0.5,
-//     scale:0.5
-// })
-
-// tl.from(".nav-bar ul li", {
-//     y:-20,
-//     opacity:0,
-//     duration:0.5,
-//     delay:0.5,
-//     scale:0.5,
-//     stagger:0.5
-// })
-
-tl.from(".hero-section h1", {
-    y:600,
+tl.from("#logo",{
     opacity:0,
-    duration:1.5,
-    delay:0.2,
-    stagger:1,
-    ease:"expo.in"
+    y:-30,
+    duration:0.5,
+    delay:0.4,
+    scale:0.5,
+    ease:"bounce.out"
 })
 
-tl.from(".hero-section h3", {
-    y:600,
+tl.from("#hello h1",{
+    x:1600,
+    duration:1,
+    delay:0.5,
+    scrollTrigger:{
+        trigger:"#hello",
+        scroller:"body",
+        markers: true,
+        scrub: 2,
+        pin: true
+    }
+})
+
+tl.from(".nav-bar ul li", {
     opacity:0,
+    y:-30,
+    duration:1,
+    delay:0.25,
+    scale:0.5,
+    ease:"bounce.out",
+    stagger:0.5
+})
+
+tl.from("#page1",{
+    opacity:0,
+    // scale:0.5,
+    duration:1,
+    ease: "power1.out",
+    x:1600
+})
+
+tl.to("#page1 .box1",{
+    scale:0.5,
     duration:2.5,
-    delay:0.2,
-    ease:"bounce",
-    stagger:1
+    delay:1,
+    borderRadius:"50%",
+    scrollTrigger:{
+        trigger:"#page1",
+        scroller:"body",
+        scrub:2,
+        end:"top -100%",
+        start:"top 0%",
+        pin:true
+    }
+
+})
+
+tl.to("#page2 h1",{
+    transform: "translateX(-100%)",
+    delay:1,
+    scrollTrigger:{
+        trigger:"#page2",
+        scroller:"body",
+        markers:true,
+        start:"top 0%",
+        end:"top -90%",
+        scrub: 2,
+        pin:true
+    }
 })
